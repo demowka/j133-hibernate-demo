2462efb91808f03625eb67c2a46b95ac784998a0
Anna Demczyk-Mazur <anna.demczyk@gmail.com>
2022-11-26 12:42:56 +0100

    Dodanie nowej encji do bazy danych

--- src/main/java/pl/sda/hibernate/demo/MAIN.java
+++ src/main/java/pl/sda/hibernate/demo/MAIN.java
@@ -1,10 +1,33 @@
 package pl.sda.hibernate.demo;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
+import java.time.LocalDate;
 
 public class MAIN {
     public static void main(String[] args) {
-        Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession();
-        session.close();
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            //Tworzymy studenta
+            Student student = new Student();
+            student.setImie("Pawel");
+            student.setDataUrodzenia(LocalDate.of(1990,1,3));
+            student.setKierunekNauczania("Informatyka");
+            student.setIndeks("123123");
+
+            //zapisujemy studenta
+            session.persist(student);
+
+            //zatwierdzamy studenta
+            transaction.commit();
+
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+
+        }
     }
 }
---
+++ src/main/java/pl/sda/hibernate/demo/TryCatchPrzykladFinally.java
@@ -0,0 +1,4 @@
+package pl.sda.hibernate.demo;
+
+public class TryCatchPrzykladFinally {
+}
