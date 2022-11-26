6d57c6712db38961210c7e0c7243f0670dc97eac
Anna Demczyk-Mazur <anna.demczyk@gmail.com>
2022-11-26 12:05:50 +0100

    Dodany model student

---
+++ src/main/java/pl/sda/hibernate/demo/Student.java
@@ -0,0 +1,25 @@
+package pl.sda.hibernate.demo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Student {
+    @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+    private  Long id;                                   //ID INT PRIMARY KEY AUTO INCREMENT
+
+    @Column(nullable = false)
+    private  String imie;                                       //imie VARCHAR 255
+    private LocalDate dataUrodzenia;                          //DATA UR DATE
+    private  String kierunekNauczania;                      //KIER N VARCHAR 255
+    private  String indeks;                                 // INDEKS VARCHAR 255
+
+    // private int wiek;                                    //wiek int not null
+    // private INTEGER wiek;                                //
+}
